72d175e73626f49fe56b4970fbe783edfb1b2eb7
raj1809 <rajtheking147@gmail.com>
2026-08-13 20:49:55 +0530

    day9: playwright setup and first test

---
+++ tests/first.spec.js
@@ -0,0 +1,14 @@
+
+    import { test, expect } from '@playwright/test'
+
+    test('Homepage has correct title', async({ page }) => {
+            await page.goto('https://www.saucedemo.com')
+            await expect(page).toHaveTitle('Swag Labs')
+
+    })
+
+    test('homepage URL contains saucedemo.com', async({ page }) => {
+        await page.goto('https://www.saucedemo.com')
+        await expect(page).toHaveURL(/saucedemo\.com/)
+
+    })
